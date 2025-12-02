9de7af476a960908cbf204ab08bca09f59970ca1
Wyatt987 <52wbedor@frhsd.com>
2025-12-02 13:42:57 -0500

    fix images halfway through step 7

--- site.js
+++ site.js
@@ -42,7 +42,40 @@ const vue_app = Vue.createApp({
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
       },
-      
+      getMonthText(dateArray) {
+            switch (dataArray[0]) {
+            case 1:
+                  return "January"
+            case 2:
+                  return "February"
+            case 3:
+                  return "March"
+            case 4:
+                  return "April"
+            case 5:
+                  return "May"
+            case 6:
+                  return "June"
+            case 7:
+                  return "July"
+            case 8:
+                  return "August"
+            case 9:
+                  return "September"
+            case 10:
+                  return "October"
+            case 11:
+                  return "November"
+            case 12:
+                  return "December"
+            }
+      },
+      like(index) {
+            this.movies[index].likes += 1
+      },
+      dislike(index) {
+            this.movies[index].dislikes += 1
+      }
 })
 
 vue_app.mount("#vue_app")
