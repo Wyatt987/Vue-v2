c1ccea3a0fb1e8019e5d80d4c64b449ce0101162
Wyatt987 <52wbedor@frhsd.com>
2025-12-02 13:04:40 -0500

    Step 3

--- site.js
+++ site.js
@@ -41,5 +41,7 @@ const vue_app = Vue.createApp({
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
       }
 })
-
+title = "IMDB + Wyatt's Top 8 Movies"
+owner = "Wyatt Bedor"
+github = "https://github.com/Wyatt987/Vue-v2"
 vue_app.mount("#vue_app")
